d9f17b8b717f9ce7281d25352aaa374e2c2fa7f3
Leon Rueda <Phlammariont@gmail.com>
2016-07-09 23:34:55 +0000

    comienzo del formulario del nuevo usuario

--- assets/js/user/app.js
+++ assets/js/user/app.js
@@ -1,6 +1,14 @@
 var app = angular.module('user', []);
 
 app.controller('userController', function ($scope){
+  $scope.showAddUserClass = "is-hide";
+  $scope.showAddUser = function () {
+    $scope.showAddUserClass = "";
+  }
+  $scope.titles = [
+    {id:0,name:"Enfermera Jefe"},
+    {id:1,name:"Enfermera Auxiliar"}
+  ]
   $scope.users =[
     {
       name:'Leon Rueda',
